310a0bd35d332f5f323920d3558b006acd2ac8b3
shifanapk <shifanapk200@gmail.com>
2025-01-22 01:52:07 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by Fathima Shifana P</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Sb</h3>
+          <h3>Copyright © {year} FS</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
